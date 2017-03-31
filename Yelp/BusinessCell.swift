e380d525d5003593a03f1f0385f3d4a8defcf8da
Virat Singh <virat.singh@airbnb.com>
2017-03-30 21:17:08 -0700

    Implemented Auto Layout in Search

--- Yelp/BusinessCell.swift
+++ Yelp/BusinessCell.swift
@@ -34,6 +34,14 @@ class BusinessCell: UITableViewCell {
         // Initialization code
         thumbImageView.layer.cornerRadius = 3
         thumbImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
